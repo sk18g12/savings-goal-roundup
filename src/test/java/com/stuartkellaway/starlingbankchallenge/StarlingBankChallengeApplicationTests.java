087cfeab75d7693b7d4a13e779c930a236b5502e
Stuart Kellaway <sk18g12@soton.ac.uk>
2020-05-27 12:21:57 +0100

    initial commit of Spring Initalizr project (https://start.spring.io/)

---
+++ src/test/java/com/stuartkellaway/starlingbankchallenge/StarlingBankChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuartkellaway.starlingbankchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlingBankChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
